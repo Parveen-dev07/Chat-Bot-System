3b4dc763199644ae84008b8061658eb000b96ad3
Parveen Choudhary <parveendev40023@gmail.com>
2026-07-21 11:33:37 +0530

    21-07-2026 p5

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,6 +3,7 @@ import userRoutes from "./routes/user.routes.js"
 import cors from "cors"
 import conversationRoutes from "./routes/conversation.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import UploadRoutes from "./routes/uploads.routes.js"
 const app = express();
 
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cors())
 app.get("/",(req,res)=>{
     res.send('route work successfullu')
 })
+app.use("/api",UploadRoutes)
 app.use("/api/user",userRoutes);
 app.use("/api/conversation",conversationRoutes);
 app.use("/api/message",messageRoutes)
