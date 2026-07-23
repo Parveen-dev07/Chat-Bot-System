e0808b460e7eaee717cb436a7d8cf88314030a1d
Parveen Choudhary <parveendev40023@gmail.com>
2026-07-23 18:31:26 +0530

    docker-setup-complete

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
 
-export const ConnectDb = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI as string);
-        console.log("DB connection successfully");
-        
-    } catch (error) {
-        console.log('DB connection failed');
-        process.exit(1)
+export const ConnectDb = async () => {
+  try {
+    const mongoUri =
+      process.env.DOCKER === "true"
+        ? process.env.DOCKER_MONGODB_URI
+        : process.env.MONGODB_URI;
+
+    console.log("DOCKER:", process.env.DOCKER);
+    console.log("MONGODB_URI:", process.env.MONGODB_URI);
+    console.log("DOCKER_MONGODB_URI:", process.env.DOCKER_MONGODB_URI);
+    console.log("SELECTED MONGO URI:", mongoUri);
+
+    if (!mongoUri) {
+      throw new Error("MongoDB URI is not defined");
     }
-}
+
+    await mongoose.connect(mongoUri);
+
+    console.log("DB connection successfully");
+  } catch (error: any) {
+    console.log("DB connection failed");
+    console.log("DB error:", error.message);
+
+    process.exit(1);
+  }
+};
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 import { ConnectDb } from "./config/db.js";
 import { initSocket } from "./sockets/socket.js";
 import http from "http"
+dotenv.config();
 const server = http.createServer(app)
 
+
+console.log("ENV TEST");
+console.log("PORT:", process.env.PORT);
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+console.log("DOCKER_MONGODB_URI:", process.env.DOCKER_MONGODB_URI);
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
 const PORT = process.env.PORT || 5000;
-dotenv.config();
 const startServer = async () => {
   await ConnectDb();
 
