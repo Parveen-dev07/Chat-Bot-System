f2d287cc18c976766063b8eb8ce91b7cb8103c76
Parveen Choudhary <parveendev40023@gmail.com>
2026-07-30 09:56:15 +0530

    vite-config-update

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "chxt-bot.duckdns.org"
+    ]
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "chxt-bot.duckdns.org"
+    ]
+  }
 })
