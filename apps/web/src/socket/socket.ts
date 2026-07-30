1974b2ad96d32e283748f25c2eb36ad73ee9424e
Parveen Choudhary <parveendev40023@gmail.com>
2026-07-30 13:23:20 +0530

    protect and use https and ssl

--- apps/web/src/socket/socket.ts
+++ apps/web/src/socket/socket.ts
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client";
 import { getToken } from "../utils/auth";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL || "http://chat-api-alb-1851760644.eu-north-1.elb.amazonaws.com",{
+export const socket = io(import.meta.env.VITE_SOCKET_URL || "https://chxt-bot.duckdns.org",{
     
 
     autoConnect:false,
